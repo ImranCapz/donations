a9db04e264660a8825d68c28eaa0cadfbf7b151b
Imran Nazeer S <149575738+ImranNazeerS@users.noreply.github.com>
2024-08-04 12:38:01 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  TextInput,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import axios from "axios";
+import ENV from "./config";
+
+export default function App() {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("Received From");
+  const [amount, setAmount] = useState(0);
+  const [currency, setCurrency] = useState("INR");
+  const [result, setResult] = useState("");
+  const { CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN } = ENV;
+
+  const SendAlert = async () => {
+    const alertData = {
+      name: name,
+      message: message,
+      identifier: "donation",
+      amount: amount,
+      currency: currency,
+    };
+
+    console.log("Alert Data:", alertData);
+
+    try {
+      const response = await axios.post(
+        "https://streamlabs.com/api/v1.0/donations",
+        alertData,
+        {
+          headers: {
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+          },
+        }
+      );
+
+      console.log("Response:", response);
+
+      if (response.data.success) {
+        setResult("Donation sent successfully!");
+      } else {
+        setResult("Failed to send donation!");
+      }
+    } catch (error) {
+      if (error.response) {
+        console.log("Error Response Data:", error.response.data);
+        console.log("Error Response Status:", error.response.status);
+        console.log("Error Response Headers:", error.response.headers);
+      } else {
+        console.log("Error Message:", error.message);
+      }
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Name</Text>
+      <TextInput
+        style={{ height: 30, width: 200, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={setName}
+        value={name}
+      />
+      <Text>Message</Text>
+      <TextInput
+        style={{ height: 30, width: 200, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={setMessage}
+        value={message}
+      />
+      <SafeAreaView style={styles.payment}>
+        <Button title="Gpay" onPress={() => setMessage("Received From GPay")} />
+        <Button
+          title="PhonePe"
+          onPress={() => setMessage("Received From PhonePe")}
+        />
+        <Button
+          title="Paytm"
+          onPress={() => setMessage("Received From Paytm")}
+        />
+      </SafeAreaView>
+      <Text>Amount</Text>
+      <TextInput
+        style={{ height: 30, width: 200, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={setAmount}
+        value={amount}
+      />
+      <Button title="Send Donation" onPress={SendAlert} />
+      <Text>{result}</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  payment: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+});
---
+++ config.js
@@ -0,0 +1,12 @@
+import Constants from "expo-constants";
+
+const ENV = {
+  dev: {
+    CLIENT_ID: "7ZL9RJRixjdyM7TZE6PmbM4Om8yP6n4M5r41qL1R",
+    CLIENT_SECRET: "FgcfKrqjnPP7hX9Hwh5wBf8SUbNc5w1qxddSIFi5",
+    ACCESS_TOKEN: "fxUhe0rxkoGFKMiSUDr1qvKVWpoVkqMgKXU7nPTi",
+  },
+};
+
+
+export default ENV;
